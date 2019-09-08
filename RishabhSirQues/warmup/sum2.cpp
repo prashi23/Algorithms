56f15b6f5d3b998465a54c4004df9db7d408ebfd
Prashi Doval <40317432+prashi23@users.noreply.github.com>
2019-09-08 12:03:35 +0530

    Add files via upload

---
+++ RishabhSirQues/warmup/sum2.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include<stdlib.h>
+#include<string.h>
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+	char *file1= argv[1], *file2=argv[2];
+	ifstream infile;
+	ofstream outfile;
+	infile.open(file1);
+	outfile.open(file2);
+	if(!file1 || !file2){
+		cout<<"unable to open file";
+	}
+	
+	int sum=0,nums[100],k=0;
+	string str;
+	while(infile >> nums[k]){
+		getline(infile,str,' ');
+		k++;
+	}
+		
+	infile.close();
+	infile.open(file1);
+	
+	getline(infile,str,'\n');
+	infile>>sum;
+	
+	for(int i=0; i<=k; i++){
+		for(int j= i+1; j<=k; j++)
+			if(nums[i]+nums[j] == sum)
+				outfile << nums[i]<<","<< nums[j]<<"\n";
+	}
+	infile.close();
+	outfile.close();
+	return 0;
+}
