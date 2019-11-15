f63d2caa518859e19cb849b0db52a96e04d083df
Prashi Doval <prashidoval23@gmail.com>
2019-11-15 06:21:13 +0530

    Create peak.cpp

---
+++ Leetcode Solutions/Find peak element/peak.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+        int findPeakElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        int low = 0;
+        int high = n - 1;
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            
+            if((mid == 0 || nums[mid] > nums[mid-1]) && (mid == n-1 || nums[mid] > nums[mid+1]))
+                return mid;
+            else if(mid < n && nums[mid] < nums[mid+1])
+                low = mid+1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+        
+};
