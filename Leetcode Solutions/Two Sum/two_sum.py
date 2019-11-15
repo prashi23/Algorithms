262119f79c62e3574b160d2af4aabfcbeb885346
Prashi Doval <prashidoval23@gmail.com>
2019-11-15 07:01:06 +0530

    Update two_sum.py

--- Leetcode Solutions/Two Sum/two_sum.py
+++ Leetcode Solutions/Two Sum/two_sum.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hmap = dict()
+        for i in range(len(nums)):
+            if nums[i] not in hmap:
+                hmap[nums[i]] = i
+        for i in range(len(nums)):
+            key = target - nums[i]
+            if (key) in hmap:
+                if hmap[key] != i:
+                    return [i, hmap[key]]
