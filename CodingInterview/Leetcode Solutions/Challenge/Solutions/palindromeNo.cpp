2d7b158581b2a298cc42386c11d3b9d5b20a05d9
Prashi Doval <prashidoval23@gmail.com>
2020-03-19 22:07:44 +0530

    Update palindromeNo.cpp

--- CodingInterview/Leetcode Solutions/Challenge/Solutions/palindromeNo.cpp
+++ CodingInterview/Leetcode Solutions/Challenge/Solutions/palindromeNo.cpp
@@ -1,1 +1,11 @@
-
+bool isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x!=0)){
+            return false;
+        }
+        int rNo = 0;
+        while(x > rNo){
+           rNo = rNo * 10 + x % 10;
+           x /= 10;
+        }
+        return x == rNo || x == rNo/10;
+    }
